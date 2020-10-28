e133e6c5653e9ccbb9a9db640a2d0a0d5ebc6e7f
Ivaylo Vladimirov <ivo@stylebit.eu>
2020-10-28 10:14:15 +0200

    WiP

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib.auth.views import logout
 
 from . import views
 
@@ -6,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login', views.login, name='login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
